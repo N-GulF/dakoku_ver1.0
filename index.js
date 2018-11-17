49bbbc81ba4cdd1ffe2606f99d3e23388b1d8718
N-GulF <shotahmku@yahoo.co.jp>
2018-11-17 12:38:08 +0900

    modify index in order to send a get request

--- index.js
+++ index.js
@@ -32,6 +32,27 @@ server.post('/webhook', line.middleware(line_config), (req, res, next) => {
         if (event.type == "message" && event.message.type == "text"){
             // ユーザーからのテキストメッセージが「こんにちは」だった場合のみ反応。
             if (event.message.text == "こんにちは"){
+                //requestをrequire
+                var request = require('request');
+
+                var webclient = require("request");
+ 
+                webclient.get({
+                    url: "https://docs.google.com/forms/d/14JZkRLaUFCkdfUXpWSt00hfaKYnXwS_QcdcaapoEJTw/formResponse",
+                         qs: {
+                           c: "0",
+                           w: "1",
+                        "entry.458666233_year": "1993",
+                        "entry.458666233_month": "10",
+                        "entry.458666233_day": "10",
+                        "entry.963651361": "めんでぃー",
+                        "entry.820423629": "てすと",
+                        "entry.551857105_hour": "10",
+                        "entry.551857105_minute": "10"
+                          }
+                    }, function (error, response, body) {
+                    console.log(body);  
+                });
                 // replyMessage()で返信し、そのプロミスをevents_processedに追加。
                 events_processed.push(bot.replyMessage(event.replyToken, {
                     type: "text",
